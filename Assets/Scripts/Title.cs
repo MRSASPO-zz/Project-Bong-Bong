f6dc5562df5f71ab6efcbf06a29a6f35d794e22a
riot94 <ghost_heartbeat@yahoo.com.sg>
2016-05-12 14:31:50 +0800

    Got the title screen somewhat working

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -6,19 +6,22 @@ using System.Timers;
 public class Title : MonoBehaviour {
     public Text title;
     // Use this for initialization
-    void Start () {
-        Timer delay = new Timer(2000);
-        title.text = "Project: ";
-        title.text += "Bong ";
-        delay.Start();
-        delay.Stop();
-        title.text += "Bong";
-        delay.Start();
-        delay.Stop();
+    void Start ()
+    {
+        StartCoroutine(intro());
     }
 	
 	// Update is called once per frame
 	void Update () {
 	    
+    }
+
+    private IEnumerator intro ()
+    {
+        title.text = "Project: ";
+        yield return new WaitForSeconds(1.5f);
+        title.text += "Bong ";
+        yield return new WaitForSeconds(1.5f);
+        title.text += "Bong";
     }
 }
