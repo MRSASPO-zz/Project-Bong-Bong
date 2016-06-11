eb6c72493f2163276414a411df7e485f5a2282c5
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-06-11 17:52:36 +0800

    Script for Dangerous Obstacles. Health UI has stopped working, Sprites for platforms have bugged out, now all platforms are just white.

---
+++ Assets/Scripts/DangerousObstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DangerousObstacle : MonoBehaviour {
+
+    Player player;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+    void OmTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            player.TakeDamage();
+        }
+    }
+
+}
