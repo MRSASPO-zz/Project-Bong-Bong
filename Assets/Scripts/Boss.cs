5209e16d63029f297477a60f876ac698afbb961e
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-07-14 19:55:23 +0800

    Built Level 2-A a bit and did Boss Health

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour
+{
+    private int bossMaxHealth = 3;
+    public int bossDamage = 0;
+
+    public int getBossHealth()
+    {
+        if (bossDamage >= bossMaxHealth)
+        {
+            return 0;
+        }else
+        {
+            return bossMaxHealth - bossDamage;
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D bomb)
+    {
+        if (bomb.tag.Equals("Bomb"))
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
+}
