619ae04870d97c5327520d8861d5d9bbfede347d
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-07-18 23:42:03 +0800

    Finished Level 2B

---
+++ Assets/Scripts/RisingLava.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RisingLava : MonoBehaviour {
+    public float speed = .1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed += .035f;
+        transform.position = new Vector3(transform.position.x, speed, transform.position.z);
+    }
+}
