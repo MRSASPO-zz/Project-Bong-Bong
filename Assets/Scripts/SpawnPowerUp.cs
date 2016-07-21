0f17ba4b0d6ec36054fc761f55801c1a426cb874
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-07-21 15:22:14 +0800

    Created prefabs for jousting knights to spawn power ups on death

---
+++ Assets/Scripts/SpawnPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPowerUp : MonoBehaviour {
+
+    public GameObject powerup;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Melee Enemy"))
+        {
+            //print("COLLIDED");
+            GameObject PU = (GameObject)Instantiate(powerup, col.transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
