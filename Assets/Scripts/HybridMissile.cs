25f947f1537fcc59eeee0e12b5831e51d7042799
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-07-22 22:33:37 +0800

    Made prefabs for Level 2-4C

---
+++ Assets/Scripts/HybridMissile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HybridMissile : MonoBehaviour
+{
+    public float speed;
+    public float autoExplodeTime = 2f;
+
+    public GameObject explosion; //prefab for explosion
+
+    void Start()
+    {
+        StartCoroutine(AutoExplode());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.isTrigger && col.CompareTag("Player"))
+        {
+            col.SendMessageUpwards("Damage");
+            ExplodeSelf();
+        }
+    }
+
+    void ExplodeSelf()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    IEnumerator AutoExplode()
+    {
+        yield return new WaitForSeconds(autoExplodeTime);
+        ExplodeSelf();
+    }
+}
---
+++ Assets/Scripts/HybridMissilePowerUp.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HybridMissilePowerUp : MonoBehaviour {
+
+
+    public float speed;
+    public float autoExplodeTime = 2f;
+    public GameObject powerup;
+    public GameObject explosion; //prefab for explosion
+
+        void Start()
+        {
+            StartCoroutine(AutoExplode());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(0, -speed, 0);
+        }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.isTrigger && col.CompareTag("Player"))
+        {
+            col.SendMessageUpwards("Damage");
+            ExplodeSelf();
+        }
+    }
+
+    void ExplodeSelf()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        GameObject PU = (GameObject)Instantiate(powerup, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    IEnumerator AutoExplode()
+    {
+        yield return new WaitForSeconds(autoExplodeTime);
+        ExplodeSelf();
+    }
+}
