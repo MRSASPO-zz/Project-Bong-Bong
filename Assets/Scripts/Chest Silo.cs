3aa9b8dd0b44dce34e43f1f65969ad8d6557f666
Ryan Tan <ghost_heartbeat@yahoo.com.sg>
2016-07-29 16:24:46 +0800

    Prepared Final Stage part 2 firing mechanic

---
+++ Assets/Scripts/Chest Silo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestSilo : MonoBehaviour {
+    private int shotsFired = 0;
+    private int shotThreshold = 10;
+    private float fireDelay = 4f;
+    private float fireDelayTimer = 0;
+    /*
+    void Update()
+    {
+        if (shotsFired < shotThreshold)
+        {
+            if (determineMissileType())
+            {
+                missilePlatforms[GetRandomPlatform()].FirePU();
+            }
+            else
+            {
+                missilePlatforms[GetRandomPlatform()].Fire();
+            }
+            shotsFired += 1;
+        }
+        else if (shotsFired == shotThreshold)
+        {
+            if (fireDelayTimer <= 0)
+            {
+                fireDelayTimer = fireDelay;
+                resetFire();
+            }
+            else
+            {
+                fireDelayTimer -= Time.deltaTime;
+            }
+        }
+    }*/
+}
