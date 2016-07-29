ddbd78907cffd8f709cbaafc7c954e8685083057
lohvht <vic94loh@hotmail.com>
2016-07-29 16:11:44 +0800

    Added a save progress checking and a level selector scene

--- Assets/Scripts/WinFlag.cs
+++ Assets/Scripts/WinFlag.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class WinFlag : MonoBehaviour {
     public string levelName;
     Animator anim;
 
     void Awake() {
+        print(SceneManager.GetActiveScene().name);
         anim = GetComponent<Animator>();
     }
 
@@ -18,7 +20,30 @@ public class WinFlag : MonoBehaviour {
     public void OnTriggerEnter2D(Collider2D hit) {
         if(hit.tag == "Player") {
             anim.Play("Win");
+            string levelString = getLevelString(SceneManager.GetActiveScene().name);
+
+            int currentChapterNo = (int)char.GetNumericValue(levelString[0]); //-1 to get the index
+            int currentLevelNo = (int)char.GetNumericValue(levelString[1]); // -1 to get the index
+            if(SceneManager.GetActiveScene().name.Equals("Scenes/Level 2-4A")) {
+                
+            } else if (SceneManager.GetActiveScene().name.Equals("Scenes/Level 2-4B")) {
+                
+            } else {
+                if (currentLevelNo + 1 > 4) {
+                    currentLevelNo = 1;
+                    currentChapterNo += 1;
+                } else {
+                    currentLevelNo += 1;
+                }
+            }
+            Game.current.levelSaveData[currentChapterNo-1][currentLevelNo-1] = true;
+            SaveLoad.Save();
             Invoke("loadLevel", 2f);
         }
+    }
+
+    private string getLevelString(string sceneName) {
+        string stripped = Regex.Replace(sceneName, @"\D", "");
+        return stripped;
     }
 }
