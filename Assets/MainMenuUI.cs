2fbb87633d59abb8edb71958cec28e1d8fdaef73
lohvht <vic94loh@hotmail.com>
2016-07-31 00:49:25 +0800

    Did sliders for music.

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+    GameObject optionObjects;
+    GameObject startObjects;
+    // Use this for initialization
+    void Start () {
+        startObjects = GameObject.FindGameObjectWithTag("ShowOnPause"); //Tag reuse
+        optionObjects = GameObject.FindGameObjectWithTag("Options");
+        hideOptions();
+    }
+
+    public void showOptions() {
+        startObjects.SetActive(false);
+        optionObjects.SetActive(true);
+    }
+
+    public void hideOptions() {
+        startObjects.SetActive(true);
+        optionObjects.SetActive(false);
+        SaveLoad.Save();
+    }
+}
