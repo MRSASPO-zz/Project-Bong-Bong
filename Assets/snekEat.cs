ebe0ea76b35569ff657ae1ba7756c2198b564658
lohvht <vic94loh@hotmail.com>
2016-07-31 13:01:09 +0800

    Added triggers for boss chase at 3-4. not completed yet

---
+++ Assets/snekEat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class snekEat : MonoBehaviour {
+    public Vector3 heightToJump;
+    public Vector3 targetPosition;
+    public Vector3 originalPosition;
+    private SpriteRenderer sr;
+    bool isUp;
+    // Use this for initialization
+    void Start () {
+        targetPosition = transform.position + heightToJump;
+        originalPosition = transform.position;
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(targetPosition, transform.position);
+        float sign = Mathf.Sign(targetPosition.y - transform.position.y);
+	    if((distance* sign) > 0) {
+            sr.flipX = false;
+            transform.Translate(heightToJump * Time.deltaTime, Space.World);
+        } else {
+            isUp = false;
+            sr.flipX = true;
+            transform.Translate(-heightToJump * Time.deltaTime, Space.World);
+        }
+	}
+}
